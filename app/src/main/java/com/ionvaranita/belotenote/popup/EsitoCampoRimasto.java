5b7dcad5e72a1dcbc89e3c91ecd86c64beb6bb2d
ion <ion.varanita18@gmail.com>
2018-05-19 22:05:48 +0200

    commit

--- app/src/main/java/com/ionvaranita/belotenote/popup/EsitoCampoRimasto.java
+++ app/src/main/java/com/ionvaranita/belotenote/popup/EsitoCampoRimasto.java
@@ -2,24 +2,24 @@ package com.ionvaranita.belotenote.popup;
 
 import com.ionvaranita.belotenote.borders.BorderedEditText;
 
-<<<<<<< HEAD
+
 public class EsitoCampoRimasto {
     private boolean eRimasto1Campo;
     private BorderedEditText campoRimasto;
-=======
+
 class EsitoCampoRimasto {
     private boolean rimasto1Campo;
     private BorderedEditText campoRimasto;
     private boolean campoFocusedAndEmpty;
 
 
->>>>>>> 1430b236ae553fc2b686409012016134ea64af67
+
 
     public EsitoCampoRimasto(){
 
     }
 
-<<<<<<< HEAD
+
     public EsitoCampoRimasto(boolean eRimasto1Campo, BorderedEditText campoRimasto) {
         this.eRimasto1Campo = eRimasto1Campo;
         this.campoRimasto = campoRimasto;
@@ -31,7 +31,7 @@ class EsitoCampoRimasto {
 
     public void seteRimasto1Campo(boolean eRimasto1Campo) {
         this.eRimasto1Campo = eRimasto1Campo;
-=======
+
     public EsitoCampoRimasto(boolean isRimasto1Campo, BorderedEditText campoRimasto) {
         this.rimasto1Campo = isRimasto1Campo;
         this.campoRimasto = campoRimasto;
@@ -43,7 +43,7 @@ class EsitoCampoRimasto {
 
     public void setRimasto1Campo(boolean rimasto1Campo) {
         this.rimasto1Campo = rimasto1Campo;
->>>>>>> 1430b236ae553fc2b686409012016134ea64af67
+
     }
 
     public BorderedEditText getCampoRimasto() {
@@ -53,8 +53,6 @@ class EsitoCampoRimasto {
     public void setCampoRimasto(BorderedEditText campoRimasto) {
         this.campoRimasto = campoRimasto;
     }
-<<<<<<< HEAD
-=======
     public boolean isCampoFocusedAndEmpty() {
         return campoFocusedAndEmpty;
     }
@@ -62,5 +60,4 @@ class EsitoCampoRimasto {
     public void setCampoFocusedAndEmpty(boolean campoFocusedAndEmpty) {
         this.campoFocusedAndEmpty = campoFocusedAndEmpty;
     }
->>>>>>> 1430b236ae553fc2b686409012016134ea64af67
 }
